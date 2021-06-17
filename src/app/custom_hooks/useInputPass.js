3dbfdd08305e878ffc83e887e0f9b32df5a1adcf
Jordan <jordanhho12@hotmail.com>
2021-06-17 15:10:37 -0700

    hotfix

--- src/app/custom_hooks/useInputPass.js
+++ src/app/custom_hooks/useInputPass.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';

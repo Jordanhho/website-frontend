30eaec7afb2c4f0e174eb40bcaf6762e7528a159
Jordan <jordanhho12@hotmail.com>
2021-06-27 23:05:27 -0700

    Add fontawesome fas library

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -15,7 +15,9 @@ import AppLayout from "./views/AppLayout";
 //add font awesome icon library
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fab);
+library.add(fas);
 
 function App() {
     return (
